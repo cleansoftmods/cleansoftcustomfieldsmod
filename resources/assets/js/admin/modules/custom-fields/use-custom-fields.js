570c5ae18d55d20d6caa2b397fe64645fd8fbd2b
Tedozi Manson <duyphan.developer@gmail.com>
2017-11-25 10:55:30 +0700

    Fix bugs

--- resources/assets/js/admin/modules/custom-fields/use-custom-fields.js
+++ resources/assets/js/admin/modules/custom-fields/use-custom-fields.js
@@ -13,15 +13,120 @@ class UseCustomFields {
          */
         this.$_EXPORT_TO = $('#custom_fields_json');
 
-        this.CURRENT_DATA = Helpers.jsonDecode(this.$_EXPORT_TO.val(), []);
-
-        console.log(this.$_EXPORT_TO.val());
+        this.CURRENT_DATA = Helpers.jsonDecode(this.base64Helper().decode(this.$_EXPORT_TO.text()), []);
 
         if (this.CURRENT_DATA) {
             this.handleCustomFields();
             this.exportData();
         }
     }
+
+    base64Helper() {
+        if (!this.base64) {
+            let Base64 = {
+                _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+                encode: function (e) {
+                    let t = "";
+                    let n, r, i, s, o, u, a;
+                    let f = 0;
+                    e = Base64._utf8_encode(e);
+                    while (f < e.length) {
+                        n = e.charCodeAt(f++);
+                        r = e.charCodeAt(f++);
+                        i = e.charCodeAt(f++);
+                        s = n >> 2;
+                        o = (n & 3) << 4 | r >> 4;
+                        u = (r & 15) << 2 | i >> 6;
+                        a = i & 63;
+                        if (isNaN(r)) {
+                            u = a = 64
+                        } else if (isNaN(i)) {
+                            a = 64
+                        }
+                        t = t + this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a)
+                    }
+                    return t
+                },
+                decode: function (e) {
+                    let t = "";
+                    let n, r, i;
+                    let s, o, u, a;
+                    let f = 0;
+                    e = e.replace(/[^A-Za-z0-9+/=]/g, "");
+                    while (f < e.length) {
+                        s = this._keyStr.indexOf(e.charAt(f++));
+                        o = this._keyStr.indexOf(e.charAt(f++));
+                        u = this._keyStr.indexOf(e.charAt(f++));
+                        a = this._keyStr.indexOf(e.charAt(f++));
+                        n = s << 2 | o >> 4;
+                        r = (o & 15) << 4 | u >> 2;
+                        i = (u & 3) << 6 | a;
+                        t = t + String.fromCharCode(n);
+                        if (u != 64) {
+                            t = t + String.fromCharCode(r)
+                        }
+                        if (a != 64) {
+                            t = t + String.fromCharCode(i)
+                        }
+                    }
+                    t = Base64._utf8_decode(t);
+                    return t
+                },
+                _utf8_encode: function (e) {
+                    e = e.replace(/rn/g, "n");
+                    let t = "";
+                    for (let n = 0; n < e.length; n++) {
+                        let r = e.charCodeAt(n);
+                        if (r < 128) {
+                            t += String.fromCharCode(r)
+                        } else if (r > 127 && r < 2048) {
+                            t += String.fromCharCode(r >> 6 | 192);
+                            t += String.fromCharCode(r & 63 | 128)
+                        } else {
+                            t += String.fromCharCode(r >> 12 | 224);
+                            t += String.fromCharCode(r >> 6 & 63 | 128);
+                            t += String.fromCharCode(r & 63 | 128)
+                        }
+                    }
+                    return t
+                },
+                _utf8_decode: function (e) {
+                    let t = "";
+                    let n = 0;
+                    let r = 0,
+                        c1 = 0,
+                        c2 = 0;
+                    while (n < e.length) {
+                        r = e.charCodeAt(n);
+                        if (r < 128) {
+                            t += String.fromCharCode(r);
+                            n++
+                        } else if (r > 191 && r < 224) {
+                            c2 = e.charCodeAt(n + 1);
+                            t += String.fromCharCode((r & 31) << 6 | c2 & 63);
+                            n += 2
+                        } else {
+                            c2 = e.charCodeAt(n + 1);
+                            let c3 = e.charCodeAt(n + 2);
+                            t += String.fromCharCode((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
+                            n += 3
+                        }
+                    }
+                    return t
+                }
+            };
+
+            this.base64 = Base64;
+        }
+
+        /**
+         * @doc
+         * There are 2 methods: encode & decode
+         */
+
+        return this.base64;
+    }
+
 
     handleCustomFields() {
         let _self = this;
@@ -312,18 +417,12 @@ class UseCustomFields {
                 case 'image':
                 case 'file':
                     customFieldData.value = $item.find('> .meta-box-wrap input').val();
-                    customFieldData.value = customFieldData.value
-                        .replace(/"/gi, "''");
                     break;
                 case 'wysiwyg':
                     customFieldData.value = WebEd.wysiwygGetContent($item.find('> .meta-box-wrap textarea'));
-                    customFieldData.value = customFieldData.value
-                        .replace(/"/gi, "''");
                     break;
                 case 'textarea':
                     customFieldData.value = $item.find('> .meta-box-wrap textarea').val();
-                    customFieldData.value = customFieldData.value
-                        .replace(/"/gi, "''");
                     break;
                 case 'checkbox':
                     customFieldData.value = [];
@@ -372,18 +471,12 @@ class UseCustomFields {
                 case 'image':
                 case 'file':
                     customFieldData.value = $item.find('> .repeater-item-input input').val();
-                    customFieldData.value = customFieldData.value
-                        .replace(/"/gi, "''");
                     break;
                 case 'wysiwyg':
                     customFieldData.value = WebEd.wysiwygGetContent($item.find('> .repeater-item-input > .lcf-wysiwyg-wrapper > .wysiwyg-editor'));
-                    customFieldData.value = customFieldData.value
-                        .replace(/"/gi, "''");
                     break;
                 case 'textarea':
                     customFieldData.value = $item.find('> .repeater-item-input textarea').val();
-                    customFieldData.value = customFieldData.value
-                        .replace(/"/gi, "''");
                     break;
                 case 'checkbox':
                     customFieldData.value = [];
--- resources/views/admin/custom-fields-boxes-renderer.blade.php
+++ resources/views/admin/custom-fields-boxes-renderer.blade.php
@@ -1,5 +1,5 @@
 <textarea name="custom_fields" id="custom_fields_json"
           class="hidden form-control"
           style="display: none !important;"
-          cols="30" rows="10">{!! $customFieldBoxes or '' !!}</textarea>
+          cols="30" rows="10">{!! isset($customFieldBoxes) ? base64_encode($customFieldBoxes) : '' !!}</textarea>
 <div id="custom_fields_container"></div>
